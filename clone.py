fb47baa0f49e5591c569c334005029827a7d3833
Andy <andylikescodes@gmail.com>
2017-05-01 03:18:00 +0000

    chenge epoch to 3

--- clone.py
+++ clone.py
@@ -32,6 +32,7 @@ model.add(Flatten(input_shape=(160,320,3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, nb_epoch=3, validation_split=0.2, shuffle=True)
+
 
 model.save('model.h5')
