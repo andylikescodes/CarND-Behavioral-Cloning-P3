819914d37bd0150987e46159f2ce28bd4603c379
Andy <andylikescodes@gmail.com>
2017-05-01 03:32:28 +0000

    used 2 laps simdata

--- clone.py
+++ clone.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 lines = []
-with open('../data/driving_log.csv') as csvfile:
+with open('../simdata/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
@@ -14,12 +14,11 @@ measurements = []
 for line in lines[1:]:
 	source_path = line[0]
 	filename = source_path.split('/')[-1]
-	current_path = '../data/IMG/' + filename
+	current_path = '../simdata/IMG/' + filename
 	image = cv2.imread(current_path)
 	images.append(image)
 	measurement = float(line[3])
 	measurements.append(measurement)
-	print (measurement)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -32,7 +31,7 @@ model.add(Flatten(input_shape=(160,320,3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, nb_epoch=3, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, nb_epoch=7, validation_split=0.2, shuffle=True)
 
 
 model.save('model.h5')
