4fbfda024dc2de281a0a7f90867da41a695157a5
Andy <andylikescodes@gmail.com>
2017-05-20 04:10:56 +0000

    use left and right cam img

--- clone.py
+++ clone.py
@@ -12,17 +12,43 @@ images = []
 measurements = []
 
 for line in lines[1:]:
-	source_path = line[0]
-	filename = source_path.split('/')[-1]
-	current_path = '../simdata/IMG/' + filename
-	image = cv2.imread(current_path)
-	images.append(image)
-	image_flipped= np.fliplr(image)
-	images.append(image_flipped)	
+	center_path = line[0]
+	left_path = line[1]
+	right_path = line[2]
+	filename_center = center_path.split('/')[-1]	
+	filename_left = left_path.split('/')[-1]
+	filename_right = right_path.split('/')[-1]
+	img_center_path = '../simdata/IMG/' + filename_center	
+	img_left_path = '../simdata/IMG/' + filename_left
+	img_right_path = '../simdata/IMG/' + filename_right
+	
+	steer_corr = 0.1
+	steer_center = float(line[3])
+	steer_left = steer_center+steer_corr
+	steer_right = steer_center-steer_corr
+	
+	image_center = cv2.imread(img_center_path)
+	image_left = cv2.imread(img_left_path)
+	image_right = cv2.imread(img_right_path)
+	images.append(image_center)
+	images.append(image_left)
+	images.append(image_right)
+	
+	measurements.append(steer_center)
+	measurements.append(steer_left)
+	measurements.append(steer_right)
 
-	measurement = float(line[3])
-	measurements.append(measurement)
-	measurements.append(-measurement)
+	image_center_flipped = np.fliplr(image_center)
+	image_left_flipped = np.fliplr(image_left)
+	image_right_flipped = np.fliplr(image_right)
+
+	images.append(image_center_flipped)	
+	images.append(image_left_flipped)
+	images.append(image_right_flipped)
+
+	measurements.append(-steer_center)
+	measurements.append(-steer_left)
+	measurements.append(-steer_right)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -46,7 +72,7 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, nb_epoch=7, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, nb_epoch=5, validation_split=0.2, shuffle=True)
 
 
 model.save('model.h5')
