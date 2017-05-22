17bda6ba56c1ca3dd12da176a2a6041a1e27b370
Andy <andylikescodes@gmail.com>
2017-05-22 18:03:32 +0000

    fixed generator

--- clone.py
+++ clone.py
@@ -26,42 +26,42 @@ def generator(samples, batch_size=32):
             
             for line in batch_samples:
                 center_path = line[0]
-                #left_path = line[1]
-                #right_path = line[2]
+                left_path = line[1]
+                right_path = line[2]
                 filename_center = center_path.split('/')[-1]	
-                #filename_left = left_path.split('/')[-1]
-                #filename_right = right_path.split('/')[-1]
+                filename_left = left_path.split('/')[-1]
+                filename_right = right_path.split('/')[-1]
                 img_center_path = '../simdata/IMG/' + filename_center	
-                #img_left_path = '../simdata/IMG/' + filename_left
-                #img_right_path = '../simdata/IMG/' + filename_right
+                img_left_path = '../simdata/IMG/' + filename_left
+                img_right_path = '../simdata/IMG/' + filename_right
                 
-                #steer_corr = 0.1
+                steer_corr = 0.1
                 steer_center = float(line[3])
-                #steer_left = steer_center+steer_corr
-                #steer_right = steer_center-steer_corr
+                steer_left = steer_center+steer_corr
+                steer_right = steer_center-steer_corr
                 
                 image_center = cv2.imread(img_center_path)
-                #image_left = cv2.imread(img_left_path)
-                #image_right = cv2.imread(img_right_path)
+                image_left = cv2.imread(img_left_path)
+                image_right = cv2.imread(img_right_path)
                 images.append(image_center)
-                #images.append(image_left)
-                #images.append(image_right)
+                images.append(image_left)
+                images.append(image_right)
                 
                 measurements.append(steer_center)
-                #measurements.append(steer_left)
-                #measurements.append(steer_right)
+                measurements.append(steer_left)
+                measurements.append(steer_right)
 
-                #image_center_flipped = np.fliplr(image_center)
-                #image_left_flipped = np.fliplr(image_left)
-                #image_right_flipped = np.fliplr(image_right)
+                image_center_flipped = np.fliplr(image_center)
+                image_left_flipped = np.fliplr(image_left)
+                image_right_flipped = np.fliplr(image_right)
 
-                #images.append(image_center_flipped)	
-                #images.append(image_left_flipped)
-                #images.append(image_right_flipped)
+                images.append(image_center_flipped)	
+                images.append(image_left_flipped)
+                images.append(image_right_flipped)
 
-                #measurements.append(-steer_center)
-                #measurements.append(-steer_left)
-                #measurements.append(-steer_right)
+                measurements.append(-steer_center)
+                measurements.append(-steer_left)
+                measurements.append(-steer_right)
 
             X_train = np.array(images)
             y_train = np.array(measurements)
@@ -97,7 +97,7 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+model.fit_generator(train_generator, samples_per_epoch=(len(train_samples)*6), validation_data=validation_generator, nb_val_samples=(len(validation_samples)*6), nb_epoch=3)
 
 
 model.save('model.h5')
