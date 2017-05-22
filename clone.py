ed7f02647d490001a266e0d238fb9128cfd0dea3
Andy <andylikescodes@gmail.com>
2017-05-22 19:30:58 +0000

    generator + lenet

--- clone.py
+++ clone.py
@@ -65,35 +65,63 @@ def generator(samples, batch_size=32):
             yield sklearn.utils.shuffle(X_train, y_train)
 
 
+
+def nvdia_net(train_generator, train_samples, validation_generator, validation_samples, n_epoch):
+
+    from keras.models import Sequential
+    from keras.layers import Flatten, Dense, Cropping2D, Lambda, Convolution2D, Dropout, MaxPooling2D
+
+    model = Sequential()
+
+    model.add(Cropping2D(cropping=((50,25),(0,0)), input_shape = (160,320,3)))
+    model.add(Lambda(lambda x: x/255 - 0.5))
+    model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='adam')
+    model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=n_epoch)
+
+
+    model.save('model.h5')
+
+
+def lenet(train_generator, train_samples, validation_generator, validation_samples, n_epoch):
+    from keras.models import Sequential
+    from keras.layers import Flatten, Dense, Cropping2D, Lambda, Convolution2D, Dropout,     MaxPooling2D
+    
+    model = Sequential()
+    model.add(Cropping2D(cropping=((25,25),(0,0)), input_shape = (160,320,3)))
+    model.add(Lambda(lambda x: x/255 - 0.5))
+    model.add(Convolution2D(6,5,5, activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(6,5,5, activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Dense(84))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    
+    model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=n_epoch)
+    model.save('model.h5')
+
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
+lenet(train_generator, train_samples, validation_generator, validation_samples, 11)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Cropping2D, Lambda, Convolution2D, Dropout, MaxPooling2D
-
-model = Sequential()
-
-model.add(Cropping2D(cropping=((50,25),(0,0)), input_shape = (160,320,3)))
-model.add(Lambda(lambda x: x/255 - 0.5))
-model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Convolution2D(64,3,3, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
-
-model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
-
-
-model.save('model.h5')
