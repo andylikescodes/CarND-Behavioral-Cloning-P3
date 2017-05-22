d21faf4cbc9675b2530a39b1bfb5bc3e6b7ebad3
Andy <andylikescodes@gmail.com>
2017-05-22 02:56:16 +0000

    roll try giving it more room on top images

--- clone.py
+++ clone.py
@@ -58,7 +58,7 @@ from keras.layers import Flatten, Dense, Cropping2D, Lambda, Convolution2D, Drop
 
 model = Sequential()
 
-model.add(Cropping2D(cropping=((75,25),(0,0)), input_shape = (160,320,3)))
+model.add(Cropping2D(cropping=((50,25),(0,0)), input_shape = (160,320,3)))
 model.add(Lambda(lambda x: x/255 - 0.5))
 model.add(Convolution2D(6,5,5, activation='relu'))
 model.add(MaxPooling2D())
